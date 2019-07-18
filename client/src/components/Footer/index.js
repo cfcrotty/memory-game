1a4f85808bd547924b2bfe307791309ee757f879
cfcrotty <carafeliselana@gmail.com>
2019-07-17 20:44:48 -0700

    update files

--- client/src/components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Footer(props) {
-  return <footer className="footer">
-    <div className="bottom">{props.title} <img alt="react" src={props.url} /></div>
-  </footer>;
-}
-
-export default Footer;
